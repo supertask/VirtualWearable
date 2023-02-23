cd471d2c456742ba63b5f0b66ecaa9e2bdd25cc9
Tasuku TAKAHASHI <lightfox.task@gmail.com>
2023-02-23 17:44:50 +0900

    Created notification UI.

---
+++ Assets/FancyScrollView/03_InfiniteScroll/Example03.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * FancyScrollView (https://github.com/setchi/FancyScrollView)
+ * Copyright (c) 2020 setchi
+ * Licensed under MIT (https://github.com/setchi/FancyScrollView/blob/master/LICENSE)
+ */
+
+using System.Linq;
+using UnityEngine;
+
+namespace FancyScrollView.Example03
+{
+    class Example03 : MonoBehaviour
+    {
+        [SerializeField] ScrollView scrollView = default;
+        private string[] itemMessages = new string[] {
+            "Alan: What's u gonna do on the weekend?",
+            "Ken: heya, are you in Berlin?",
+            "Installing LINE, WhatsUp ..",
+            "Microsoft Bing: You're in! Welcome to the new Bing!",
+            "Fitbit: Your Charge 5 battery level is low",
+            "Altman: yeah, I will be there.",
+        };
+
+        void Start()
+        {
+            var items = Enumerable.Range(0, itemMessages.Length)
+                .Select(i => new ItemData($"{itemMessages[i]}"))
+                .ToArray();
+
+            scrollView.UpdateData(items);
+            scrollView.SelectCell(0);
+        }
+    }
+}
